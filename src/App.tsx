1600242f0a8ef12024641c529709335f907cddbc
Manan Joshi <mananjoshi1995@gmail.com>
2022-01-23 00:45:48 -0600

    Adding redaxios and react-query packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = (): React.ReactElement => {
+  const [, setSearchTerm] = useState('');
+
   return (
-    <div className='flex flex-col items-center justify-center w-full h-full p-4 bg-slate-300'>
-      <div className='grid grid-cols-3 lg:max-w-screen-lg'>
-        <div className='flex flex-col'>
-          <div className='mb-2 text-3xl font-extrabold'>Vi</div>
-          <div className='text-xl'>Vite</div>
-        </div>
-        <div className='flex flex-col'>
-          <div className='mb-2 ml-1 text-3xl font-extrabold'>Re</div>
-          <div className='text-xl'>React</div>
-        </div>
-        <div className='flex flex-col text-center'>
-          <div className='mb-2 text-3xl font-extrabold'>Tail</div>
-          <div className='text-xl'>Tailwind CSS</div>
-        </div>
-      </div>
-      <div className='mt-6'>
-        <ul className='tracking-wide list-disc'>
-          <li>React</li>
-          <li>Tailwind CSS v3</li>
-          <li>Vite</li>
-          <li>Eslint</li>
-          <li>Prettier</li>
-          <li>PNPM</li>
-        </ul>
+    <div className='flex flex-col items-center justify-center w-full h-full p-4 bg-slate-100'>
+      <div className='flex items-center'>
+        <input
+          className='px-4 py-2 mr-6 border rounded-md shadow-md'
+          onChange={(e) => {
+            setSearchTerm(e.currentTarget.value);
+          }}
+        />
+        <button className='p-1 text-sm rounded-md bg-cyan-600 text-slate-50'>
+          Search
+        </button>
       </div>
     </div>
   );
